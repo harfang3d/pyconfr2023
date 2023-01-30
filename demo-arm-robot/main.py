2e0c45e15eb27e3a22c0555d23d84aedc1599886
fra <fra@movida-mail.com>
2023-01-30 17:03:36 +0100

    WIP interactive GUI.

--- demo-arm-robot/main.py
+++ demo-arm-robot/main.py
@@ -5,7 +5,7 @@ import harfang as hg
 hg.InputInit()
 hg.WindowSystemInit()
 
-res_x, res_y = 800, 800
+res_x, res_y = 1024, 720
 win = hg.NewWindow("PyConFr2023", res_x, res_y, 32, hg.WV_Windowed)
 hg.RenderInit(win, hg.RT_OpenGL)
 hg.RenderReset(res_x, res_y, hg.RF_MSAA4X | hg.RF_MaxAnisotropy)
@@ -32,8 +32,21 @@ for i in range(node_list.size()):
 	print(node_name)
 	if node_name.startswith("_PIVOT_"):
 		axis = node_name.split("_")[-1]
-		pivot_name = node_name.split("_")[1] + " " + node_name.split("_")[2]
-		axis_list.append({"pivot": pivot_name, "axis": axis, "node": node})
+		if node_name.find("FINGER") > -1:
+			pivot_name = node_name.split("_")[2] + " " + node_name.split("_")[3] + node_name.split("_")[4]
+		else:
+			pivot_name = node_name.split("_")[1] + " " + node_name.split("_")[2]
+
+		_rot = node.GetTransform().GetRot()
+		if axis == "X":
+			angle = hg.RadianToDegree(_rot.x)
+		elif axis == "Y":
+			angle = hg.RadianToDegree(_rot.y)
+		else:
+			angle = hg.RadianToDegree(_rot.z)
+		angle_min = angle - 90.0
+		angle_max = angle + 90.0
+		axis_list.append({"pivot": pivot_name, "axis": axis, "node": node, "angle": angle, "min": angle_min, "max": angle_max})
 		print(axis)
 
 hg.ImGuiInit(10, imgui_prg, imgui_img_prg)
@@ -51,10 +64,23 @@ while not hg.ReadKeyboard().Key(hg.K_Escape) and hg.IsWindowOpen(win):
 	# view_id = view_id + 1
 	hg.ImGuiBeginFrame(res_x, res_y, hg.TickClock(), hg.ReadMouse(), hg.ReadKeyboard())
 
-	if hg.ImGuiBegin('Robot Arm'):
+	if hg.ImGuiBegin('Robot Arm', True, hg.ImGuiWindowFlags_NoMove | hg.ImGuiWindowFlags_NoResize):
+		hg.ImGuiSetWindowPos('Robot Arm', hg.Vec2(res_x / 40, res_y - (res_y / 2.45)), hg.ImGuiCond_Once)
+		hg.ImGuiSetWindowSize('Robot Arm', hg.Vec2(res_x / 3, res_y / 2.5), hg.ImGuiCond_Once)
 		hg.ImGuiText('Axis control')
 		for axis in axis_list:
-			hg.ImGuiText(axis["pivot"] + "(" + axis["axis"] + ")")
+			# hg.ImGuiText(axis["pivot"] + "(" + axis["axis"] + ")")
+			angle_update , axis["angle"] = hg.ImGuiSliderFloat(axis["pivot"] + "(" + axis["axis"] + ")", axis["angle"], axis["min"], axis["max"]) 
+			if angle_update:
+				_trs = axis["node"].GetTransform()
+				_rot = _trs.GetRot()
+				if axis["axis"] == "X":
+					_rot.x = hg.DegreeToRadian(axis["angle"])
+				elif axis["axis"] == "Y":
+					_rot.y = hg.DegreeToRadian(axis["angle"])
+				else:
+					_rot.z = hg.DegreeToRadian(axis["angle"])
+				_trs.SetRot(_rot)
 	hg.ImGuiEnd()
 
 	hg.SetView2D(view_id, 0, 0, res_x, res_y, -1, 1, hg.CF_None, hg.Color.Black, 1, 0)
