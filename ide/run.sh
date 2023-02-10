2a12ed6fcb3b38f21e42e76b4640326a5057a185
pmp-p <mail.peny@free.fr>
2023-02-10 17:29:06 +0100

    "ide" starter script

---
+++ ide/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "
+
+
+
+    for brave goto brave://flags/  search for file-system-access-api and enable
+
+
+
+"
+
+if [ -f "/opt/appimage/Chromium.AppImage" ]
+then
+    BROWSER=/opt/appimage/Chromium.AppImage
+else 
+    wget -c https://apprepo.de/appimage/download/chromium --output-document=Chromium.AppImage
+    chmod +x ./Chromium.AppImage
+    BROWSER=$(pwd)/Chromium.AppImage
+fi
+
+
+$BROWSER \
+ --password-store=basic  --disable-cookie-encryption --user-data-dir=$(pwd)/webdev.pro \
+ --no-default-browser-check --test-type  --auto-open-devtools-for-tabs \
+ --aggressive-cache-discard --disable-notifications --disable-remote-fonts \
+ --disable-voice-input --enable-aggressive-domstorage-flushing --disable-shared-workers \
+ --disable-features=PreloadMediaEngagementData,MediaEngagementBypassAutoplayPolicies \
+ --enable-features=Vulkan,file-system-access-api --enable-unsafe-webgpu --enable-webgpu-developer-feature \
+ http://localhost:8000
+
